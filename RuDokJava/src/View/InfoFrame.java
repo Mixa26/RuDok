36022a2249e428e4536017e853b7bb63111230d2
Mihajlo <djmixa26.hockey@gmail.com>
2021-10-25 23:00:25 +0200

    Dodata slika na Info dialog

--- RuDokJava/src/View/InfoFrame.java
+++ RuDokJava/src/View/InfoFrame.java
@@ -2,10 +2,12 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.ImageObserver;
 
 public class InfoFrame extends JDialog {
 
-    private Label student;
+    private ImageIcon studentPicture;
+    private JLabel student;
 
     public InfoFrame(Frame parent, String title, boolean modal)
     {
@@ -13,7 +15,8 @@ public class InfoFrame extends JDialog {
         setSize(new Dimension(500,500));
         setLocationRelativeTo(parent);
 
-        student = new Label("Mihajlo Madzarevic RN 55/20 ");
+        studentPicture = new ImageIcon("image/mixa.jpg");
+        student = new JLabel("Mihajlo Madzarevic RN 55/20 ", studentPicture, JLabel.CENTER);
 
         add(student);
 
